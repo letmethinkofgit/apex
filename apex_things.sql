f6673ada8b5a1a96939862e7c0764605298494b7
letmethinkofgit <83652892+letmethinkofgit@users.noreply.github.com>
2021-05-22 23:04:55 +0530

    Update apex_things.sql

--- apex_things.sql
+++ apex_things.sql
@@ -1,3 +1,5 @@
+
+====================
 If you want to set value of page item in Javascript :
 
 apex.item("P1_NEW").setValue("something"); -- This will not set the value of the page item P1_NEW in the session. This will only be displayed on the page.
@@ -7,3 +9,14 @@ apex.server.process ( "SAVE_HIDDEN_VALUE_IN_SESSION_STATE", {
   x01: "set_session_state",
   pageItems: "#P1_NEW"
   }, {dataType: 'text'} );
+
+
+===================
+Get the content of the page without opening page in javascript
+
+var url = $v('P1_DYNAMIC_URL');
+$.get(url, function(html) {
+    htmlObject = html;
+    });
+arr = htmlObject; -- html object will be an array.
+var myJSON = JSON.stringify(htmlObject); -- Used to stringigy array into string
